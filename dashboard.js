c9bd3a30120b10e611e73210ac210fb0a0605735
NET24BD <284401873+NET24BD@users.noreply.github.com>
2026-08-01 17:30:50 +0600

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,30 +1,71 @@
-// Card Page Open
+// Card Redirect
 
 function openPage(page){
 
-    window.location.href = page;
+window.location.href = page;
 
 }
 
 
 
-// Login থেকে user data নিলে এখানে দেখাবে
 
-let username = localStorage.getItem("username");
+// Profile Dropdown
 
-let photo = localStorage.getItem("photo");
+function toggleProfileMenu(){
+
+let menu=document.getElementById("profileMenu");
+
+
+if(menu.style.display==="block"){
+
+menu.style.display="none";
+
+}
+
+else{
+
+menu.style.display="block";
+
+}
+
+
+}
+
+
+
+
+// Logout
+
+function logout(){
+
+localStorage.clear();
+
+window.location.href="login.html";
+
+}
+
+
+
+
+
+// Load User Data
+
+let username=localStorage.getItem("username");
+
+let photo=localStorage.getItem("photo");
 
 
 
 if(username){
 
-    document.getElementById("username").innerHTML = username;
+document.getElementById("username").innerHTML=username;
 
 }
 
 
+
 if(photo){
 
-    document.getElementById("profileImg").src = photo;
+document.getElementById("profileImg").src=photo;
 
 }
