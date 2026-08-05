964dc2a6575de75d525168c41b33f4582c0e1796
NET24BD <284401873+NET24BD@users.noreply.github.com>
2026-08-05 16:29:09 +0600

    Update login.js

--- login.js
+++ login.js
@@ -1,122 +1,183 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbx2-w0CK4IXldQfzUxjOTpN2m2knTH858fr8vMmcowbecL6UQ9oJVcAyoMMLb8GYbY/exec";
+const API_URL =
+"https://script.google.com/macros/s/AKfycbx2-w0CK4IXldQfzUxjOTpN2m2knTH858fr8vMmcowbecL6UQ9oJVcAyoMMLb8GYbY/exec";
 
 
-const loginForm = document.getElementById("loginForm");
-const message = document.getElementById("message");
-const loginBtn = document.getElementById("loginBtn");
 
+const form=document.getElementById("loginForm");
 
-loginForm.addEventListener("submit", function(e){
+const message=document.getElementById("message");
 
-    e.preventDefault();
+const button=document.getElementById("loginBtn");
 
 
-    const username = document.getElementById("username").value.trim();
-    const password = document.getElementById("password").value.trim();
 
+form.addEventListener("submit",function(e){
 
-    if(username === "" || password === ""){
 
-        message.style.color = "red";
-        message.innerHTML = "Enter username and password";
-        return;
+e.preventDefault();
 
-    }
 
 
-    loginBtn.disabled = true;
-    loginBtn.innerHTML = "CHECKING...";
+let username=
+document.getElementById("username").value.trim();
 
 
-    fetch(API_URL, {
 
-        method: "POST",
+let password=
+document.getElementById("password").value.trim();
 
-        headers:{
-            "Content-Type":"text/plain;charset=utf-8"
-        },
 
-        body: JSON.stringify({
 
-            username: username,
-            password: password
 
-        })
+button.innerHTML="CHECKING...";
 
-    })
+button.disabled=true;
 
 
-    .then(response => response.json())
 
 
-    .then(data => {
+fetch(API_URL,{
 
+method:"POST",
 
-        console.log(data);
+headers:{
 
+"Content-Type":"text/plain;charset=utf-8"
 
-        if(data.success === true){
+},
 
 
-            localStorage.setItem("isLogin","true");
+body:JSON.stringify({
 
-            localStorage.setItem("username",data.username);
+username:username,
 
-            localStorage.setItem("name",data.name);
+password:password
 
-            localStorage.setItem("role",data.role);
+})
 
-            localStorage.setItem("picture",data.picture);
 
+})
 
 
-            message.style.color="green";
-            message.innerHTML="Login Successful";
+.then(res=>res.json())
 
 
-            setTimeout(function(){
+.then(data=>{
 
-                window.location.href="dashboard.html";
 
-            },1000);
+console.log(data);
 
 
 
-        }
+if(data.success){
 
-        else{
 
 
-            message.style.color="red";
-            message.innerHTML=data.message;
+localStorage.setItem("isLogin","true");
 
+localStorage.setItem("username",data.username);
 
-        }
+localStorage.setItem("name",data.name);
 
+localStorage.setItem("role",data.role);
 
-    })
+localStorage.setItem("picture",data.picture);
 
 
-    .catch(error=>{
 
+message.style.color="green";
 
-        console.log(error);
+message.innerHTML="Login Success";
 
 
-        message.style.color="red";
-        message.innerHTML="Server Error";
 
+setTimeout(()=>{
 
-    })
 
+window.location.href="dashboard.html";
 
-    .finally(()=>{
 
-        loginBtn.disabled=false;
-        loginBtn.innerHTML="LOGIN";
+},1000);
 
-    });
 
+
+}
+
+else{
+
+
+message.style.color="red";
+
+message.innerHTML=data.message;
+
+
+}
+
+
+})
+
+
+.catch(err=>{
+
+
+console.log(err);
+
+
+message.style.color="red";
+
+message.innerHTML="Server Connection Error";
+
+
+})
+
+
+.finally(()=>{
+
+
+button.disabled=false;
+
+button.innerHTML="LOGIN";
 
 
 });
+
+
+});
+
+
+
+
+
+// Password Show Hide
+
+
+document
+.getElementById("togglePassword")
+.onclick=function(){
+
+
+let pass=document.getElementById("password");
+
+
+if(pass.type==="password"){
+
+
+pass.type="text";
+
+this.innerHTML='<i class="fa-solid fa-eye-slash"></i>';
+
+
+}
+
+else{
+
+
+pass.type="password";
+
+this.innerHTML='<i class="fa-solid fa-eye"></i>';
+
+
+}
+
+
+};
