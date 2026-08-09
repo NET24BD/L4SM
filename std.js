7bea61a0df6f63d3f6b93629cba4e85458fda4d9
NET24BD <284401873+NET24BD@users.noreply.github.com>
2026-08-09 16:30:00 +0600

    Create std.js

---
+++ std.js
@@ -0,0 +1,20 @@
+/* =========================================
+   DASHBOARD JS
+========================================= */
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    console.log("Dashboard Loaded");
+
+});
+
+
+/* =========================================
+   OPEN PAGE
+========================================= */
+
+function openPage(page) {
+
+    window.location.href = page;
+
+}
